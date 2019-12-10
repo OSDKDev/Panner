40473da4fdb3f57ee52490df0cd34ec746871f0c
Dunning-Kruger <hello@dunningkruger.dev>
2019-12-10 11:12:40 -0500

    Added methods for entity wide definitions

---
+++ src/Sort/Generators/ByPropertyName/PEntityBuilder.AllPropertiesAreSortableByName.cs
@@ -0,0 +1,22 @@
+﻿namespace Panner.Builders //Namespace is intentional so extensions are picked up when builder is used.
+{
+    using System.Reflection;
+    using Panner.Sort.Generators;
+
+    public static partial class PEntityBuilderExtensions
+    {
+        /// <summary>Marks all properties of an entity as sortable, referenced by its name.</summary>
+        public static PEntityBuilder<T> AllPropertiesAreSortableByName<T>(this PEntityBuilder<T> pEntityBuilder)
+            where T : class
+        {
+            var generator = pEntityBuilder.GetOrCreateGenerator<ISortParticle<T>, ByPropertyNameParticleGenerator<T>>();
+
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var p in properties)
+                generator.Add(p);
+
+            return pEntityBuilder;
+        }
+    }
+}
